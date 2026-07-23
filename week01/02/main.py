0742c70d9ca387b7e38d21901f4b9b197740b993
dlwleasy <165463130+dlwleasy@users.noreply.github.com>
2026-07-23 16:30:33 +0900

    edit1
    코드 수정

--- week01/02/main.py
+++ week01/02/main.py
@@ -1,16 +1,30 @@
 
+from pprint import pprint
 
 try:
-    with open('mission_computer_main.log','r',encoding='utf-8') as f:
-        logs = f.read()
-        # print(logs)
+    with open('mission_computer_main.log','r',encoding='utf-8') as file:
+        logs = file.read()
+        print(logs)
+
+
 except FileNotFoundError:
     print("Error: The log file does not exist.")
 
-# change to list 
-# 리스트 안에  timestamp,event,message 각각이 담기게 정리
 
-log_list=logs.split("/n")
-print(log_list)
-sorted_list=log_list.sort()
-reversed_list=sorted_list.reverse()
+# 첫째줄 제외 이중 리스트로 날짜 구분 및 (,) 기준 정보 구분하여 리스트 생성
+
+log_list = logs.splitlines()[1:] # print(log_list)
+list=[]
+for line in log_list:
+ list.append(line.split(","))
+# print(list)
+
+# 시간 역순으로 정렬 list.reverse()
+list.reverse()
+pprint(list,width=100)
+
+# 사전 객체 전환 : 리스트의 값에 무엇을 의미하는지 키(이름) 지정
+
+
+
+# mission_computer_main.json 파일로 저장하는데 파일 포멧은 JSON(JavaScript Ontation)으로 저장
