83239a452bd7a6280873b679d35ac721ed1b6ecc
dlwleasy <165463130+dlwleasy@users.noreply.github.com>
2026-07-24 18:02:00 +0900

    forth commit

--- week01/04/design_dome.py
+++ week01/04/design_dome.py
@@ -1,9 +1,12 @@
+
+
+
 # 1. 결정되는거 : 재질, 지름,두께,면적
 # 반구체의 돔 면적 구하기
 def sphere_area(diameter, material, thickness=1):
     radius = diameter / 2
     area = 2 * 3.14159 * (radius ** 2)  # 반구체 면적 공식: 2πr^2
-    return area
+    return {"재질": material,"지름":diameter, "면적": round(area, 3), "두께": thickness}
 
 # material,diameter = 사용자 입력
 # 기본값 material=유리, thickness=1
@@ -28,8 +31,8 @@ except ValueError:
     print("지름은 숫자로 입력해야 합니다.")
 
 # 구하기 - 면적
-area=sphere_area(diameter=diameter, material=material)
-print(f"반구체의 돔 면적은 {area:.3f}입니다.")
+info=sphere_area(diameter=diameter, material=material)
+# print(f"반구체의 돔 면적은 {area:.3f}입니다.")
 
 # 2. 무게 재계산
 
@@ -40,12 +43,23 @@ weight_on_mars = {}
 def calculate_weight_on_mars(weight_on_earth):
     print("화성에서의 무게")
     for name,weight in weight_on_earth.items():
-        # weight_on_earth[weight] = weight_on_earth[weight] * (3.71 / 9.81)  # 화성 중력 가속도 / 지구 중력 가속도
-        weight_on_mars[name] =round(weight * 0.378, 3) # 화성 중력 가속도 / 지구 중력 가속도
+        #(3.71 / 9.81)  # 화성 중력 가속도 / 지구 중력 가속도
+        weight_on_mars[name] =round(weight * 0.378, 3) 
         print(f" {name} : {weight_on_mars[name]:.3f}입니다.")
     
     return weight_on_mars
 
 weight=calculate_weight_on_mars(weight_on_earth)
-print(weight)
-  
+# print(weight)
+
+
+# 전역변수에 지정
+materal=info["재질"]
+diameter=info["지름"]
+thickness=info["두께"]
+area=info["면적"]
+weight=weight[materal]
+print(f"재질 : {materal}, 지름 : {diameter}, 두께 : {thickness}, 면적 : {area}, 무게 : {weight}")
+
+
+
