174d40e0a631f71268499be7586e63b692de11e5
dlwleasy <165463130+dlwleasy@users.noreply.github.com>
2026-07-25 10:16:37 +0900

    fix : 구체적인 예외 처리

--- week01/01/main.py
+++ week01/01/main.py
@@ -5,10 +5,9 @@ try:
         logs = f.read()
         print(logs)
 except FileNotFoundError:
-    print("Error: The log file does not exist.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+    print("해당 파일이 존재하지 않습니다.")
 except PermissionError:
     print("파일에 접근할 권한이 없습니다.")
 except Exception as e:
-    print(f"알 수 없는 에러가 발생했습니다: {e}")
+    print(f"예상치 못한 오류가 발생했습니다.: {e}")
+
