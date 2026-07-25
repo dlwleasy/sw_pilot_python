5699d3606b7f71ce59a4fc26211a8431df9c867f
dlwleasy <165463130+dlwleasy@users.noreply.github.com>
2026-07-25 10:45:29 +0900

    fix : log.md파일 작성 받기

--- week01/01/main.py
+++ week01/01/main.py
@@ -1,3 +1,4 @@
+#1
 print("Hello Mars")
 
 try:
@@ -10,4 +11,15 @@ except PermissionError:
     print("파일에 접근할 권한이 없습니다.")
 except Exception as e:
     print(f"예상치 못한 오류가 발생했습니다.: {e}")
+
+
+user_text=input("파일을 분석한 내용을 입력하세요 : ")
+
+with open('log_analysis.md','w',encoding='utf-8') as f:
+    f.write(user_text)
+
+print("완료")
+
+
+
 
