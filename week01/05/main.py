e022135eb92fbd5a5651bf905ab2b1bad2e6dac4
dlwleasy <165463130+dlwleasy@users.noreply.github.com>
2026-07-28 02:43:39 +0900

    edit : 예외처리

--- week01/05/main.py
+++ week01/05/main.py
@@ -11,20 +11,20 @@ arr3 = np.loadtxt(file_list[2],dtype='str', delimiter=',', encoding='utf-8', ski
 parts = np.vstack((arr1,arr2,arr3))
 
 # 3 
-# 합 구하기 => 같은 항목끼리 묶기 
 # -> 공통요소 = 위치, 이름 -> 위치 : 값[i :: 100] / 이름 & 값의 위치 동일 
 
 keys = parts[:,0]
 values = parts[:,1].astype(int)
 # 3-1 parts_to_work_on.csv
-with open("parts_to_work_on.csv", mode="w", newline="", encoding="utf-8") as file:
+try:
+ with open("parts_to_work_on.csv", mode="w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
  
     for i in range(100):
      mean = np.mean(values[i::100])  
-
      if 50.0 > (mean):
       writer.writerow([keys[i],round(mean,3)])
-
     print("저장 완료")
 
+except OSError as e:
+    print(f"파일 저장 중 오류가 발생했습니다: {e}")
