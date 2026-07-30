7e745cdae34e1925b8ff5056ceaf7ee04555231f
dlwleasy <165463130+dlwleasy@users.noreply.github.com>
2026-07-30 17:43:25 +0900

    fix : 리스트 수정
    append 함수의 잘못된 사용

--- week01/06/main.py
+++ week01/06/main.py
@@ -1,27 +1,36 @@
 import random
 
 class DummySensor:
+
     env_values={
-        'mars_base_internal_temperature':null,
+
+        'mars_base_internal_temperature':random_data[0],
         'mars_base_external_temperature':,
         'mars_base_internal_humidity':,
         'mars_base_external_illuminance':,
         'mars_base_internal_co2':,
         'mars_base_internal_oxygen':,
     }
+    
 
     def set_env():
-        random_data=[]
-        random_data=random_data.append(random.uniform(18,30))
-        random_data=random_data.append(random.uniform(0,21))
-        random_data=random_data.append(random.uniform(50,60))
-        random_data=random_data.append(random.uniform(500,715))
-        random_data=random_data.append(random.uniform(0.02,0.1))
-        random_data=random_data.append(random.uniform(4,7))
+        random_data = [
+        random.uniform(18, 30),
+        random.uniform(0, 21),
+        random.uniform(50, 60),
+        random.uniform(500, 715),
+        random.uniform(0.02, 0.1),
+        random.uniform(4, 7),
+        ]
+        return random_data
 
     def get_env():
         return env_values
 
+
+# ummySensor 클래스를 ds라는 이름으로 인스턴스(Instance)로 만든다.인스턴스화 한 DummySensor 클래스에서 set_env()와 get_env()를 차례로 호출해서값을 확인한다.
+
+# 전체 코드를 mars_mission_computer.py 파일로 저장한다.
 
 ds=DummySensor() #instance
 set=ds.set_env()
@@ -29,7 +38,7 @@ get=ds.get_env()
 
 try:
     with open("mars_mission_computer.py","w",encoding='utf-8') as file:
-        file
+        file.writer()
 except Exception as e:
     print(f"예상치 못한 오류가 발생했습니다.: {e}")
 
